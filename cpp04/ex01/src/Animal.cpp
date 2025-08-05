de1714e82d5c9eff25f4032dca8aef32e7544623
Duarte Eusébio <dde-carv@student.42lisboa.com>
2025-08-05 14:49:28 +0100

    On the works.

--- cpp04/ex01/src/Animal.cpp
+++ cpp04/ex01/src/Animal.cpp
@@ -1,6 +1,8 @@
 #include "Animal.hpp"
 
-Animal::Animal() : _type("Animal")
+/************ Constructors and Destructor ************/
+
+Animal::Animal() : _type("animal")
 {
 	std::cout << GREEN << "Animal default constructor called." << RST << std::endl;
 }
@@ -8,6 +10,7 @@ Animal::Animal() : _type("Animal")
 Animal::Animal(const Animal &object)
 {
 	*this = object;
+
 	std::cout << GREEN << "Animal copy constructor called" << RST << std::endl;
 }
 
@@ -16,27 +19,38 @@ Animal::~Animal()
 	std::cout << RED << "Animal default destructor called." << RST << std::endl;
 }
 
+/************ Setter ************/
+
 void	Animal::setType(std::string type)
 {
 	_type = type;
 }
+
+/************ Getter ************/
 
 std::string	Animal::getType() const
 {
 	return _type;
 }
 
+/************ Operator ************/
+
 Animal	&Animal::operator=(const Animal &copy)
 {
-	if(this != &copy)
+	if(this == &copy)
 	{
 		if(this->_type != copy._type)
 			_type = copy._type;
 	}
+	else
+		_type = copy._type;
+
 	std::cout << YELLOW << "Animal copy assignment operator called" << RST << std::endl;
 
 	return *this;
 }
+
+/************ Member Function ************/
 
 void	Animal::makeSound() const
 {
