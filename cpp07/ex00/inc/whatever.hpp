809786eea2e75bc720fdcff586acb7ffe8ba3d19
Duarte Eusébio <dde-carv@student.42lisboa.com>
2025-10-23 14:25:53 +0100

    CPP07 ex00 done.

--- cpp07/ex00/inc/whatever.hpp
+++ cpp07/ex00/inc/whatever.hpp
@@ -6,7 +6,32 @@
 /*   By: dde-carv <dde-carv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 13:48:44 by dde-carv          #+#    #+#             */
-/*   Updated: 2025/10/23 13:48:45 by dde-carv         ###   ########.fr       */
+/*   Updated: 2025/10/23 14:11:10 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#pragma once
+
+#include <iostream>
+
+template <typename T>
+void	swap(T &x, T &y)
+{
+	T tmp;
+
+	tmp = x;
+	x = y;
+	y = tmp;
+}
+
+template <typename T>
+T	min(T x, T y)
+{
+	return (x < y) ? x : y;
+}
+
+template <typename T>
+T	max(T x, T y)
+{
+	return (x > y) ? x : y;
+}
--- cpp07/ex00/src/main.cpp
+++ cpp07/ex00/src/main.cpp
@@ -6,7 +6,28 @@
 /*   By: dde-carv <dde-carv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/23 13:48:46 by dde-carv          #+#    #+#             */
-/*   Updated: 2025/10/23 13:48:47 by dde-carv         ###   ########.fr       */
+/*   Updated: 2025/10/23 14:07:08 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "../inc/whatever.hpp"
+
+int main()
+{
+	int a = 2;
+	int b = 3;
+
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << "\n" << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
