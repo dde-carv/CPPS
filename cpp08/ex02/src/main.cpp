9fe182d472e117d1da48ba8cfd50ddcb345ec7a2
Duarte Eusébio <dde-carv@student.42lisboa.com>
2025-10-29 10:31:01 +0000

    Testing CPP08.

--- cpp08/ex02/src/main.cpp
+++ cpp08/ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dde-carv <dde-carv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/28 08:51:48 by dde-carv          #+#    #+#             */
-/*   Updated: 2025/10/28 10:26:41 by dde-carv         ###   ########.fr       */
+/*   Updated: 2025/10/29 10:30:30 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,17 +32,34 @@ int	main()
 
 	MutantStack<int>::iterator it = mstack.begin();
 	MutantStack<int>::iterator ite = mstack.end();
+	MutantStack<int>::reverse_iterator rit = mstack.rbegin();
+	MutantStack<int>::reverse_iterator rite = mstack.rend();
 
 	++it;
+	std::cout << "Iterator after 1 iteration: " << *it << std::endl;
 	--it;
 
-	std::cout << "How many elements in stack: " << mstack.size() << std::endl;
+	++rit;
+	std::cout << "Reverse iterator after 1 iteration: " << *rit << std::endl;
+	--rit;
+
+	std::cout << "How many elements in stack: " << mstack.size() << "\n" << std::endl;
 	std::cout << "Iterating throught the stack:" << std::endl;
 	while (it != ite)
 	{
 		std::cout << *it << std::endl;
 		++it;
 	}
+
+	std::cout << std::endl;
+
+	std::cout << "Reverse iterating throught the stack:" << std::endl;
+	while (rit != rite)
+	{
+		std::cout << *rit << std::endl;
+		++rit;
+	}
+
 	std::stack<int> s(mstack);
 	return 0;
 }
