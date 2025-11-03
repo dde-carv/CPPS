65fbe9d39cafd210e4777f393449686815f4a7ce
Duarte Eusébio <dde-carv@student.42lisboa.com>
2025-11-03 17:13:06 +0000

    Working on CPP09 ex00.

--- cpp09/ex00/inc/BitcoinExchange.hpp
+++ cpp09/ex00/inc/BitcoinExchange.hpp
@@ -6,14 +6,16 @@
 /*   By: dde-carv <dde-carv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 10:07:40 by dde-carv          #+#    #+#             */
-/*   Updated: 2025/10/30 11:32:59 by dde-carv         ###   ########.fr       */
+/*   Updated: 2025/11/03 15:45:36 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #pragma once
 
-#include <iostream>
 #include <map>
+#include <iostream>
+#include <fstream>
+#include <cmath>
 
 class BitcoinExchange
 {
@@ -22,13 +24,14 @@ class BitcoinExchange
 
 	public:
 		BitcoinExchange();
+		BitcoinExchange(const BitcoinExchange &other);
+		BitcoinExchange	&operator=(const BitcoinExchange &other);
 		~BitcoinExchange();
+
+		void		loadDatabase(const std::string &fileName);
+		void		processDatabase(const std::string &fileName) const;
+
+	private:
+		bool		validDate(const std::string &date);
+		std::string	trim(const std::string &str) const;
 };
-
-BitcoinExchange::BitcoinExchange()
-{
-}
-
-BitcoinExchange::~BitcoinExchange()
-{
-}
--- cpp09/ex00/src/BitcoinExchange.cpp
+++ cpp09/ex00/src/BitcoinExchange.cpp
@@ -6,10 +6,33 @@
 /*   By: dde-carv <dde-carv@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 10:07:42 by dde-carv          #+#    #+#             */
-/*   Updated: 2025/10/30 10:07:57 by dde-carv         ###   ########.fr       */
+/*   Updated: 2025/11/03 15:51:51 by dde-carv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/BitcoinExchange.hpp"
 
+BitcoinExchange::BitcoinExchange()
+{
+}
 
+BitcoinExchange::BitcoinExchange(const BitcoinExchange &other)
+{
+	*this = other;
+}
+
+BitcoinExchange	&BitcoinExchange::operator=(const BitcoinExchange &other)
+{
+	if (this != &other)
+		_dataBase = other._dataBase;
+	return *this;
+}
+
+BitcoinExchange::~BitcoinExchange()
+{
+}
+
+void	BitcoinExchange::loadDatabase(const std::string &fileName)
+{
+	
+}
